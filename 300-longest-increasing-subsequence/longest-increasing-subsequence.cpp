fc1ab8a7311246226809b245e8396679d31f081a
Pawanpundir10 <122386400+Pawanpundir10@users.noreply.github.com>
2025-07-18 10:38:31 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (64.29%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,24 +1,51 @@
 class Solution {
 public:
+
+int ceilidx(vector<int>&tail,int begin,int end,int target)
+  {
+      while(end>begin)
+      {
+          int mid=begin+(end-begin)/2;
+          if(tail[mid]>=target)
+          {
+              end=mid;
+          }
+          else
+          {
+              begin=mid+1;
+          }
+      }
+      
+      return end;
+  }
+  
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         if(n==0)
         {
             return 0;
         }
-        vector<int>dp(n,1);
+        vector<int>tail(n);
+        int len=1;
+        
+        tail[0]=nums[0];
+        
         for(int i=1;i<n;i++)
         {
-            for(int j=0;j<i;j++)
+            if(nums[i]>tail[len-1])
             {
-                if(nums[i]>nums[j])
-                {
-                    dp[i]=max(dp[i],1+dp[j]);
-                }
+                tail[len]=nums[i];
+                len++;
+            }
+            
+            else
+            {
+                int c=ceilidx(tail,0,len-1,nums[i]);
+                tail[c]=nums[i];
             }
         }
-        int maxlen=*max_element(dp.begin(),dp.end());
-
-        return maxlen;
+        
+        return len;
+        
     }
 };
