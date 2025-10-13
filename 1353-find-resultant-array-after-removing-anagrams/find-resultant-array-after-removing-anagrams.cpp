a59d887ff300f873b1b0550a762c60c57a8dc120
Pawanpundir10 <122386400+Pawanpundir10@users.noreply.github.com>
2025-10-14 01:27:22 +0530

    Time: 4 ms (32.85%) | Memory: 16.1 MB (75.50%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string> ans;
+        ans.push_back(words[0]); // keep the first word
+
+        for (int i = 1; i < words.size(); i++) {
+            string prev = ans.back();
+            string curr = words[i];
+
+            sort(prev.begin(), prev.end());
+            sort(curr.begin(), curr.end());
+
+            if (prev != curr) { 
+                ans.push_back(words[i]);
+            }
+        }
+
+        return ans;
+    }
+};
