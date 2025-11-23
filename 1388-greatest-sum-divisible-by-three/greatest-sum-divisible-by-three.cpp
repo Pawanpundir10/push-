106f209d1412dc39e39ddad66c63b99c4dadbf09
Pawanpundir10 <122386400+Pawanpundir10@users.noreply.github.com>
2025-11-24 00:54:00 +0530

    Time: 43 ms (27.66%) | Memory: 45.9 MB (18.28%) - LeetSync

---
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(int i, int remainder, vector<int>& nums, vector<vector<int>>& t) {
+        if (i >= nums.size()) {
+            return (remainder == 0) ? 0 : INT_MIN;
+        }
+
+        if (t[i][remainder] != -1)
+            return t[i][remainder];
+
+        int pick = nums[i] + solve(i + 1, (remainder + nums[i]) % 3, nums, t);
+        int notPick = solve(i + 1, remainder, nums, t);
+
+        return t[i][remainder] = max(pick, notPick);
+    }
+
+    int maxSumDivThree(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> t(n, vector<int>(3, -1));
+        return solve(0, 0, nums, t);
+    }
+};
