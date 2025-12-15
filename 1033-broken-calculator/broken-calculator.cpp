06c0e22bc86c542645e5070929c402d52c6e061f
Pawanpundir10 <122386400+Pawanpundir10@users.noreply.github.com>
2025-12-15 22:10:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (82.57%) - LeetSync

---
+++ 1033-broken-calculator/broken-calculator.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        if(target<=startValue)
+        {
+            return abs(target-startValue);
+        }
+
+        if(target%2==0)
+        {
+            return 1+brokenCalc(startValue,target/2);
+        }
+        else{
+            return 1+brokenCalc(startValue,target+1);
+        }
+    }
+};
